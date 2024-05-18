8bd2509b35c86a72b251f7b3acbba9bab4b51abf
Ico-code <nico.nikusti@gmail.com>
2024-05-18 21:30:02 +0300

    finnished tutorials

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'dashboard/customers',
+};
+
+
 export default function page(){
     return <p>Customers Page</p>;
 }
