97e2010a44489868dd6bbc1f8625dd55ad38c462
Ico-code <nico.nikusti@gmail.com>
2024-05-19 11:06:03 +0300

    fixed search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,9 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function HandleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
-    const params = new URLSearchParams(searchParams);
+  const handleSearch = useDebouncedCallback((e: React.ChangeEvent<HTMLInputElement>)=>{
+    const params = new URLSearchParams(searchParams)
+    const term: string = e.target.value
     params.set('page', '1');
     if (term) {
       params.set('query', term);
@@ -19,8 +19,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 300);
-  }
+  }, 300)
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -29,9 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          HandleSearch(e.target.value);
-        }}
+        onChange={handleSearch}
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
